0b89e7d0f5a3d2f21f8ab32aba607c2d5537c8b8
Drew Ross <42723693+AndrewGallowitch@users.noreply.github.com>
2018-09-14 18:10:56 -0400

    final upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,23 +7,23 @@ var userNumber = 0;
 
 var wins = 0;
 var losses = 0;
+var counter = 0;
 
 
 // functions
 // function to restart game to call when user wins or loses
 
-
-var reset = function() {
+var reset = function () {
     targetNumber = Math.floor(Math.random() * 100) + 20;
     $("#number-to-guess").text(targetNumber);
-    
+
     $("#soccer").val(Math.floor(Math.random() * 12) + 1);
     $("#football").val(Math.floor(Math.random() * 12) + 1);
     $("#tennisball").val(Math.floor(Math.random() * 12) + 1);
     $("#basketball").val(Math.floor(Math.random() * 12) + 1);
     userNumber = 0;
-    $("#user-number").html(userNumber);
-    
+    $("#user-number").html(userNumber); 
+
     console.log($("#soccer").val());
     console.log($("#football").val());
     console.log($("#basketball").val());
@@ -31,43 +31,40 @@ var reset = function() {
 }
 reset()
 
-
-
+ function newGameWin () {
+    if (wins) {
+        alert("You win! Play Again?");
+    }
+ }
+    
+function newGameLose () {
+    if (losses) {
+        alert("Sorry you suck! Be better! Play Again?");
+    }
+}
 
 // on.click function for images to pull the numberOptions
-$(".btn").on("click", function() {
-    var number = parseInt($(this).val());
-    userNumber += number;
+$(".btn").on("click", function () {
+    counter = parseInt($(this).val());
+    userNumber += counter;
     $("#user-number").html(userNumber);
-    
+
     //win if statement
-if(userNumber === targetNumber) {
-    wins++;
-    $("#wins").html(wins);
-    reset();
-}
+    if (userNumber === targetNumber) {
+        wins++;
+        $("#wins").html(wins);
+        newGameWin();
+        reset();
+        
+    }
     //lose if statement
-else if(userNumber > targetNumber) {
-    losses++;
-    $("#losses").html(losses);
-    reset();
-}
-    
+    if (userNumber > targetNumber) {
+        losses++;
+        $("#losses").html(losses);
+        newGameLose();
+        reset();
+       
+    }
+
 });
 
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-  
